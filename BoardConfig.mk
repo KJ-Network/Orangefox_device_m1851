1e8db4e7ce77d318e62c79e9dce278d69333c36d
knkj55 <knkj55@proton.me>
2025-08-30 06:26:46 +0800

    BoardConfig: set TW_BRIGHTNESS_PATH correctly
    To fix issue: Cannot adjust brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # TWRP specific build flags
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/wled/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
